4d33c3db2d678daa6a1ffd4aeb60e5666c9272f8
Dola Barai <dola43dc@gmail.com>
2023-05-04 01:24:48 +0600

    section updated

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layouts/Main";
-import Home from "../Pages/Home/Home/Home";
 import Blog from "../Pages/Blog/Blog";
 import Login from "../Pages/Login/Login";
 import Register from "../Pages/Home/Register/Register";
@@ -14,14 +13,7 @@ const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
-        errorElement: <ErrorPage></ErrorPage>,
-        children: [
-            {
-                path: '/',
-                element: <Home></Home>
-            },
-    
-        ],
+        errorElement: <ErrorPage></ErrorPage>
     },
 
     {
